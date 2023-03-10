1eaa83ea01ba6fc23ca04162d612d72f9e8c48ec
rhesus2016 <rhesus2016@gmail.com>
2023-03-11 05:47:46 +0900

    feature: 계산기 초기화 버튼 구현

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import "./index.css";
 
-const button = document.getElementById("button");
+const buttons = document.getElementsByClassName("btn");
+const result = document.querySelector(".result");
+let expressions: string[] = [];
 
-button.addEventListener("click", () => {
-  alert("clicked!");
-});
+const handleResetClick = () => {
+  result.textContent = "0";
+  expressions = [];
+
+  console.log(result.textContent);
+  console.log(expressions);
+};
+
+const handleCalculationClick = () => {
+  if (!expressions.length) return;
+};
+
+const handleOperatorClick = (button: Element) => {};
+
+const isNum = (str: string) => {
+  if (isNaN(Number(str))) return false;
+  else return true;
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  if (buttons[i].textContent === "C") {
+    buttons[i].addEventListener("click", handleResetClick);
+  } else if (buttons[i].textContent === "+/-") {
+  } else if (buttons[i].textContent === "%") {
+  } else if (buttons[i].textContent === "=") {
+    buttons[i].addEventListener("click", handleCalculationClick);
+  } else {
+    buttons[i].addEventListener("click", () => handleOperatorClick(buttons[i]));
+  }
+}
